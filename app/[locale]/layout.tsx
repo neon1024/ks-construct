b295c8ff5356cfe4bf5dea3cfde4f80051371714
neon1024 <robertscumnpu@gmail.com>
2026-09-13 23:48:08 +0300

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,24 +1,29 @@
+import { notFound } from "next/navigation";
+
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 
-import type { Locale } from "@/i18n/config";
+import { locales, type Locale } from "@/i18n/config";
 import getTranslations from "@/i18n/getTranslations";
 
 import { AppContextProvider } from "@/contexts/AppContext";
 
-type Props = {
-    children: React.ReactNode;
-    params: Promise<{
-        locale: Locale;
-    }>;
-};
-
-export default async function LocaleLayout({ children, params }: Props) {
+export default async function LocaleLayout({
+    children,
+    params,
+}: LayoutProps<"/[locale]">) {
     const { locale } = await params;
-    const translations = await getTranslations(locale);
+
+    if (!locales.includes(locale as Locale)) {
+        notFound();
+    }
+
+    const typedLocale = locale as Locale;
+
+    const translations = await getTranslations(typedLocale);
 
     return (
-        <AppContextProvider locale={locale} translations={translations}>
+        <AppContextProvider locale={typedLocale} translations={translations}>
             <Header />
             <main>{children}</main>
             <Footer />
